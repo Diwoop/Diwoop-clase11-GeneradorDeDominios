732003ad1a87de3bd951ef314655f877f2a79d52
Diwoop <144156230+Diwoop@users.noreply.github.com>
2024-06-12 19:25:24 +0000

    generador de dominios

---
+++ app.js
@@ -0,0 +1,18 @@
+let opcionesPronoun = ['the', 'our', 'your', 'their'];
+let opcionesAdj = ['great', 'big', 'messy', 'flawless'];
+let opcionesNoun = ['jogger', 'racoon', 'company', 'cat','coffee'];
+let opcionesDominio= [".com", ".net", ".us", ".io"];
+
+function dominiosRandom(){
+    for (let i = 0; i < opcionesPronoun.length; i++){
+        for (let j = 0; j < opcionesAdj.length; j++){
+            for (let k = 0; k < opcionesNoun.length; k++){
+                for (let g = 0; g < opcionesDominio.length; g++){
+                    console.log(`${opcionesPronoun[i]}${opcionesAdj[j]}${opcionesNoun[k]}${opcionesDominio[g]}`);
+                }
+            }
+        }
+    }
+}
+dominiosRandom();
+
