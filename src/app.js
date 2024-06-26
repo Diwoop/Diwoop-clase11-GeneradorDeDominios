6d0259d935ba1406076ba0ddc90e19e2ee35b8f3
Diwoop <144156230+Diwoop@users.noreply.github.com>
2024-06-26 18:45:19 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let opcionesPronoun = ['the', 'our', 'your', 'their'];
+let opcionesAdj = ['great', 'big', 'messy', 'flawless'];
+let opcionesNoun = ['jogger', 'racoon', 'company', 'cat','coffee'];
+let opcionesDominio= [".com", ".net", ".us", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function dominiosRandom(){
+    for (let i = 0; i < opcionesPronoun.length; i++){
+        for (let j = 0; j < opcionesAdj.length; j++){
+            for (let k = 0; k < opcionesNoun.length; k++){
+                for (let g = 0; g < opcionesDominio.length; g++){
+                    console.log(`${opcionesPronoun[i]}${opcionesAdj[j]}${opcionesNoun[k]}${opcionesDominio[g]}`);
+                }
+            }
+        }
+    }
+}
+dominiosRandom();
